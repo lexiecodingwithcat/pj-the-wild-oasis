ec1980ae012bc7cf2c1373b7e4b86ecadfa13d3a
Tianzi Cui <cuitianzi0@gmail.com>
2024-08-13 23:40:41 -0600

    set up pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row"
+import Login from "./pages/Login";
+import Bookings from "./pages/Bookings";
+import Account from "./pages/Account";
+import Cabins from "./pages/Cabins";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import PageNotFound from "./pages/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/account", element: <Account /> },
+      { path: "/bookings", element: <Bookings /> },
+      { path: "/cabins", element: <Cabins /> },
+      { path: "/settings", element: <Settings /> },
+      { path: "/users", element: <Users /> },
+    ],
+  },
+  { path: "/login", element: <Login /> },
+  { path: "*", element: <PageNotFound /> },
+]);
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>
-        {/* using "as" prop so that the styled component will know which HTML element need to be rendered exactly */}
-        <Row type="horizontal">
-          <Heading as="h1">The Wild Oasis</Heading>
-          <Heading as="h2">Check in and out</Heading>
-        </Row>
-        <Row>
-          <Heading as="h3">Form</Heading>
-          <Button variation="primary" size="medium">Check in</Button>
-          <Button  variation="secondary" size="large">Check out</Button>
-        </Row>
-      </div>
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
