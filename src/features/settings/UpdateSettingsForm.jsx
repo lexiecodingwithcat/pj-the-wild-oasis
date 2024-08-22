3af49c52b2fc9aa7ac32ad55d635b384d6fa1921
Tianzi Cui <cuitianzi0@gmail.com>
2024-08-22 12:30:16 -0600

    updated app settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,8 +1,11 @@
+import { useForm } from "react-hook-form";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Spinner from "../../ui/Spinner";
 import { useSettings } from "./useSettings";
+import { useUpdateSettings } from "./useUpdateSettings";
+
 
 function UpdateSettingsForm() {
   const {
@@ -14,27 +17,66 @@ function UpdateSettingsForm() {
     } = {},
     isLoading,
   } = useSettings();
+  const { isUpdating, updateSettings } = useUpdateSettings();
+  const { register } = useForm();
+
   if (isLoading) return <Spinner />;
+
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+    if (!value) return;
+    // [field] is to pass the params dynamically
+    updateSettings({ [field]: value });
+  }
+
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
+        <Input
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLength}
+          disabled={isUpdating}
+          //when using register to control the form, React Hook Form will manage all event handler, so it's better to use it inside the register
+          {...register("minBookingLength", {
+            onBlur: (e) => handleUpdate(e, "minBookingLength"),
+          })}
+        />
       </FormRow>
+
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          disabled={isUpdating}
+          {...register("maxBookingLength", {
+            onBlur: (e) => handleUpdate(e, "maxBookingLength"),
+          })}
+        />
       </FormRow>
+
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
           defaultValue={maxGuestPerBooking}
+          disabled={isUpdating}
+          {...register("maxGuestPerBooking", {
+            onBlur: (e) => handleUpdate(e, "maxGuestPerBooking"),
+          })}
         />
       </FormRow>
+
       <FormRow label="Breakfast price">
         <Input
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
+          disabled={isUpdating}
+          {...register("breakfastPrice", {
+            onBlur: (e) => handleUpdate(e, "breakfastPrice"),
+          })}
         />
       </FormRow>
     </Form>
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -4,6 +4,7 @@ import UpdateSettingsForm from "../features/settings/UpdateSettingsForm";
 
 function Settings() {
   return (
+
     <Row>
       <Heading as="h1">Update hotel settings</Heading>
       <UpdateSettingsForm />
